6c5fb2b5b79d8f0c48f3f94ca2b502ca5cd8018f
vp1001 <vedantp28@gmail.com>
2023-08-02 12:10:46 +0530

    /start /info commands added

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const logger = winston.createLogger({                                   //logger
 
 const {TOKEN, SERVER_URL} = process.env                                 //saving Token ans Server url from environment variables file
 const T_API = 'https://api.telegram.org/bot'+TOKEN;                     //API for Telegram bot, not to be disclosed
-const URI = '/webhook/'+TOKEN;                                          //
+const URI = '/webhook/'+TOKEN;                                          // https://wikibot-kwb5.onrender.com
 const W_URL = SERVER_URL+URI;                                           //webhook URL
 
 var Title;
@@ -42,7 +42,25 @@ app.post(URI, async (req, res) => {                                     //for bo
         sendersChatID = req.body.message.chat.id;
         logger.info('Message Text : '+msg+'\n\t  From : '+req.body.message.from.first_name+'\n\t  Chat ID : '+sendersChatID);
         console.log('Message Text : '+msg+'\n\t  From : '+req.body.message.from.first_name+'\n\t  Chat ID : '+sendersChatID);
-        if(msg != "")
+        if(msg == "/start")
+        {
+            text = "Hello "+req.body.message.from.first_name+", Welcome to WikiBot😁 Please type the keyword which you want to search and let me do the rest for you. For more information, type /info."
+            const SendURL = T_API+'/sendMessage?chat_id='+sendersChatID+'&text='+text;
+            const response = await ex.post(SendURL)
+            logger.info('Reply sent : '+text)
+            console.log('Reply sent : '+text)
+            wiki_random_search = 'https://en.wikipedia.org/wiki?search='
+        }
+        else if (msg == "/info")
+        {
+            text = "Hello "+req.body.message.from.first_name+", I am WikiBot. I can fetch you any existing wikipedia page. All you need to do us to type what you want to search for and hit the send button. I will fetch the wikipedia page and bring it to you!"
+            const SendURL = T_API+'/sendMessage?chat_id='+sendersChatID+'&text='+text;
+            const response = await ex.post(SendURL)
+            logger.info('Reply sent : '+text)
+            console.log('Reply sent : '+text)
+            wiki_random_search = 'https://en.wikipedia.org/wiki?search='
+        }
+        else if(msg != "")
         {
             wiki_random_search = wiki_random_search+msg;
             const wiki_result = await ex.get(wiki_random_search);
@@ -59,7 +77,7 @@ app.post(URI, async (req, res) => {                                     //for bo
                 text = 'No wikipedia page found. Please check spelling and try again';
             }
 
-            if(req.body.message.from.first_name == 'Riya')
+            if(req.body.message.from.first_name == 'Riyaa')
             {
                 const SendURL = T_API+'/sendMessage?chat_id=1069308235&text='+msg;
                 const response = await ex.post(SendURL)
